4045adb645d0dc48881c1b7d646668a780b147bd
quirinezonneveld <144068154+quirinezonneveld@users.noreply.github.com>
2023-09-23 14:54:45 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,25 @@
 // JavaScript Document
 console.log("hi");
+
+
+
+
+
+// navigatie melding
+let navigatieMelding = document.querySelector('header div:first-of-type p');
+let navigatieMeldingText = ['Vandaag besteld, maandag in huis', 'Gratis verzending vanaf 39 euro', 'Gratis afhalen in 27 winkels'];
+let index = 0;
+
+function updateText() {
+    navigatieMelding.textContent = navigatieMeldingText[index];
+    index = (index + 1) % navigatieMeldingText.length;
+    //  % navigatieMeldingText.length zorgt ervoor dat index niet buiten de grenzen van array valt
+}
+function animateText() {
+    updateText(); // Start met de eerste tekst
+    setInterval(updateText, 2000); 
+  }
+animateText();
+
+/* textContent:
+https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent */
